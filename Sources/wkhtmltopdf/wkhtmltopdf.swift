d4955e3bb73bedf0a366db08dd9bcf632af24aba
Amos Cheng <amos.cheng.gpj@gmail.com>
2020-10-07 12:18:47 +0800

    clean

---
+++ Sources/wkhtmltopdf/wkhtmltopdf.swift
@@ -0,0 +1,45 @@
+//
+//  wkhtmltopdf.swift
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2020 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+public struct WKHtml2Pdf {
+    
+    private static let binary = "/usr/local/bin/wkhtmltopdf"
+    
+    public var parameters: [Parameter] = []
+    
+    public func generate(pages: [Page]) -> Data {
+        
+        let process = Process()
+        let stdout = Pipe()
+        process.launchPath = WKHtml2Pdf.binary
+        
+        process.arguments = self.parameters.flatMap { $0.values }
+        process.arguments?.append("-")
+        process.standardOutput = stdout
+        process.launch()
+        
+        return stdout.fileHandleForReading.readDataToEndOfFile()
+    }
+}
